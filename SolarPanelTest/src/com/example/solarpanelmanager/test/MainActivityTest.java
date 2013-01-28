771993ea0357eec067e7d792359e1daef31bf96f
Sean Hurley <sean.hurley6@gmail.com>
2013-01-27 18:26:00 -0600

    Adding initial project and test project

---
+++ SolarPanelTest/src/com/example/solarpanelmanager/test/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.example.solarpanelmanager.test;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+	// Just a dummy test so stuff runs
+	public void testMain() {
+		assertTrue(true);
+	}
+}
