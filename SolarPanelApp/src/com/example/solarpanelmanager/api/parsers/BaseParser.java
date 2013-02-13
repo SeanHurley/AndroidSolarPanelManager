96983c3a97235a8f5e5493cdec6e0304f2518fed
Sean Hurley <sean.hurley6@gmail.com>
2013-02-13 11:35:19 -0600

    Added a base parser that is just stubbed

---
+++ SolarPanelApp/src/com/example/solarpanelmanager/api/parsers/BaseParser.java
@@ -0,0 +1,13 @@
+package com.example.solarpanelmanager.api.parsers;
+
+import java.util.HashMap;
+
+public class BaseParser {
+
+	public HashMap<String, Object> parseResponse(String response) {
+		HashMap<String, Object> values = new HashMap<String, Object>();
+
+		return values;
+	}
+
+}
