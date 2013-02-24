27d20fb4e3345a5678641cde5abb76c7c4f11e45
Soo Woo <soohwoo@gmail.com>
2013-02-24 15:47:34 -0600

    Created Main Menu of App

--- SolarPanelApp/src/com/example/solarpanelmanager/MainActivity.java
+++ SolarPanelApp/src/com/example/solarpanelmanager/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.solarpanelmanager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,46 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// BUTTON: Connect to existing device
+		final Button buttonConnect = (Button) findViewById(R.id.button_connect_device);
+		buttonConnect.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	
+		// Add a new device
+		final Button buttonAddDevice = (Button) findViewById(R.id.button_add_device);
+		buttonConnect.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		// Settings
+		final Button buttonSettings = (Button) findViewById(R.id.button_settings);
+		buttonConnect.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		// About
+		final Button buttonAbout = (Button) findViewById(R.id.button_about);
+		buttonConnect.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});	
 	}
 
 	@Override
