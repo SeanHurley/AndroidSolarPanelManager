0f6a2374b428a3c5f8c07e7cd03159fba501c59e
Sean Hurley <sean.hurley6@gmail.com>
2013-03-10 18:45:37 -0500

    Removed old stuff

--- SolarPanelApp/src/com/example/solarpanelmanager/api/parsers/APIKeys.java
+++
@@ -1,5 +0,0 @@
-package com.example.solarpanelmanager.api.parsers;
-
-public class APIKeys {
-	public static String RESULT = "result";
-}
--- SolarPanelApp/src/com/example/solarpanelmanager/api/parsers/ResponseParser.java
+++ SolarPanelApp/src/com/example/solarpanelmanager/api/parsers/ResponseParser.java
@@ -2,7 +2,6 @@ package com.example.solarpanelmanager.api.parsers;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
@@ -16,20 +15,6 @@ import com.example.solarpanelmanager.api.responses.SnapshotResponse;
 import com.example.solarpanelmanager.api.responses.ViewChargeConstraintsResponse;
 
 public class ResponseParser {
-
-	/**
-	 * @param response
-	 *            The response that will be given from the bluetooth
-	 *            communication layer
-	 * @return A mapping of just plain KEY-VALUE information in a HashMap. The
-	 *         object types may still need to be parsed as this will just be
-	 *         pulling raw from the responses
-	 */
-	private static HashMap<String, Object> parseResponse(String response) {
-		HashMap<String, Object> values = new HashMap<String, Object>();
-
-		return values;
-	}
 
 	public static BaseResponse parseBasicResponse(String response) {
 		// Call the parse response for the basic map, and then create the proper
