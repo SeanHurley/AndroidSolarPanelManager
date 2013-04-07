1fff3039e06da5af1794e1f7c7838e50ee99bb26
Aaron Bercovici <bercovi1@illinois.edu>
2013-04-07 16:41:06 -0500

    basic calendar again

--- SolarPanelApp/src/com/example/calendar/BasicCalendar.java
+++ SolarPanelApp/src/com/example/calendar/BasicCalendar.java
@@ -1,5 +1,26 @@
 package com.example.calendar;
 
-public class BasicCalendar {
+import java.util.Date;
+import java.util.Map;
 
+import com.example.solarpanelmanager.api.responses.Event;
+
+public class BasicCalendar {
+	private Map<String, Event> calendar;
+	
+	@SuppressWarnings("deprecation")
+	public void addEvent(Event event){
+		long first = event.getFirstTime();
+		Date newDay = new Date(first);
+		long newStart = newDay.getHours() * 60 + newDay.getMinutes();
+		String key = day.getHours() + ":" + day.getMinutes();
+		boolean conflict = false;
+		for(Event e : calendar.values()){
+			Date oldDay = new Date(e.getFirstTime());
+			long oldStart = oldDay.getHours() * 60 + oldDay.getMinutes();
+			long oldEnd = oldStart + e.getDuration();
+			
+			
+		}
+	}
 }
