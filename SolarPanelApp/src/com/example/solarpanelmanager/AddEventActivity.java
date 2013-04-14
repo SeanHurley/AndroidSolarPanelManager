8e3e4cef13240a9c5400acbef41783f24b66b9e6
Jim Bright <bright7@illinois.edu>
2013-04-14 17:00:31 -0500

    Changed time parsing in add activity

--- SolarPanelApp/src/com/example/solarpanelmanager/AddEventActivity.java
+++ SolarPanelApp/src/com/example/solarpanelmanager/AddEventActivity.java
@@ -1,12 +1,13 @@
 package com.example.solarpanelmanager;
 
-import java.sql.Time;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,14 +39,35 @@ public class AddEventActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
 				String start = startTime.getText().toString();
 				String duration = durationText.getText().toString();
 				String interval = intervalText.getText().toString();
 				String name = nameText.getText().toString();
 				
-				Time startTime = Time.valueOf(start);
-				Time durationLength = Time.valueOf(duration);
-				Time intervalTime = Time.valueOf(interval);
+				Date startTime = null;
+				try {
+					startTime = df.parse(start);
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				Date durationLength = null;
+				try {
+					durationLength = df.parse(duration);
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				Date intervalTime = null;
+				try {
+					intervalTime = df.parse(interval);
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				
 				Event e = new Event("some id", name, startTime.getTime(), durationLength.getTime(), intervalTime.getTime());
 				Intent i = new Intent();
