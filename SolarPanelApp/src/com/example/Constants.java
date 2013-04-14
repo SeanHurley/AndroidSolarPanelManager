cf30d3216587024d0108f5ccb0a9478652698608
Jim Bright <bright7@illinois.edu>
2013-04-14 16:21:15 -0500

    Added AddEventActivity

--- SolarPanelApp/src/com/example/Constants.java
+++ SolarPanelApp/src/com/example/Constants.java
@@ -3,4 +3,5 @@ package com.example;
 public class Constants {
 	public static final String POWER_USER_PREFERENCE = "power_user";
 	public static final String CURRENT_DEVICE = "current_device";
+	public static final String EVENT_RESULT_CODE = "event_result_code";
 }
--- SolarPanelApp/src/com/example/solarpanelmanager/api/responses/Event.java
+++ SolarPanelApp/src/com/example/solarpanelmanager/api/responses/Event.java
@@ -1,6 +1,8 @@
 package com.example.solarpanelmanager.api.responses;
 
-public class Event {
+import java.io.Serializable;
+
+public class Event implements Serializable {
 
 	private String id;
 	private String name;
