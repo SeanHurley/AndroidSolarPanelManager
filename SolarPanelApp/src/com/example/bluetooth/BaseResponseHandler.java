88bd6ea7972d202b94b3f29a9c0a55b841bc4bed
Sean Hurley <sean.hurley6@gmail.com>
2013-04-15 19:18:22 -0500

    Added short method comment

--- SolarPanelApp/src/com/example/bluetooth/BaseResponseHandler.java
+++ SolarPanelApp/src/com/example/bluetooth/BaseResponseHandler.java
@@ -3,6 +3,12 @@ package com.example.bluetooth;
 import com.example.solarpanelmanager.api.parsers.ResponseParser;
 import com.example.solarpanelmanager.api.responses.BaseResponse;
 
+/**
+ * @author seanhurley
+ * 
+ *         Simply overrides the Base parseResponse so that that functionality
+ *         can be shared throughout several different handlers
+ */
 public abstract class BaseResponseHandler extends CommunicationHandler<BaseResponse> {
 
 	public BaseResponseHandler(Callback<BaseResponse> callback, String target, String pass) {
