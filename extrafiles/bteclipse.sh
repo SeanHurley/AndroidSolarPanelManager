1224fc9a3a0b694490c47db46ce55cc3b0c47942
Sean Hurley <sean.hurley6@gmail.com>
2013-04-28 15:16:52 -0500

    Added the extrafiles for the documentation.

---
+++ extrafiles/bteclipse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "$0" )" && pwd )
+export DYLD_LIBRARY_PATH=$DIR
+# Change this to be wherever you have eclipse installed
+/Users/seanhurley/Downloads/eclipse/eclipse
